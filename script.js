2b0f441d7a05d856a03958123ae4eb0f83ec1aee
Faviola Caballero <fabycg18@gmail.com>
2020-10-09 14:57:30 -0700

    completed getweatherdata function to return json promise

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
  * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
+ * TODO: Complete getWeatherData() to return json response Promise ✅
  * TODO: Complete searchCity() to get user input and get data using getWeatherData()
  * TODO: Complete showWeatherData() to set the data in the the html file from response
  */
@@ -12,13 +12,17 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
  * Retrieve weather data from openweathermap
  * HINT: Use fetch()
  * HINT: URL should look like this:
+ * first param after ? is the city, second is the app id which is the api_key while the third is in units.
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
-
-  //CODE GOES HERE
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  });
 };
 
 /**
